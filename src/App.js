1b8bcc3a4b2c6d08f9f486f91e8d1f8c3538fd6d
izik4004 <izik4004@gmail.com>
2022-04-23 00:03:06 +0100

    delete functionality

--- src/App.js
+++ src/App.js
@@ -24,10 +24,20 @@ const App = () => {
         reminder: true,
     },
 ])
+
+// deletae task 
+
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task) => task.id !== id ))
+}
   return (
     <div className="container">
         <Header title='Task Tracker'/>
-        <Tasks tasks={tasks}/>
+        {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask}/>
+        ) : (
+          'No Task to show'
+        )}
     </div>
   );
 }
