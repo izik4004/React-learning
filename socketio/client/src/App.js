3f502694587b1997688b5af78957f8f65db1c143
izik4004 <izik4004@gmail.com>
2022-07-16 21:19:59 +0100

    socket.io

--- socketio/client/src/App.js
+++ socketio/client/src/App.js
@@ -1,6 +1,21 @@
+import {useEffect} from "react";
 import './App.css';
+import io from "socket.io-client"
+const socket = io.connect("http://localhost:3001");
+
 
 function App() {
+
+  const sendMessage = () => {
+    socket.emit("send_message", {message: "Hello"})
+  };
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+        alert(data.message);
+    });
+  }, [socket])
+
   return (
     <div className="App">
       <input placeholder='Message......'/>
