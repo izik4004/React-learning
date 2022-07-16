874b47f928d8bf412ee5ccb063dc4849a3c0835d
izik4004 <izik4004@gmail.com>
2022-07-16 21:44:08 +0100

    changes

--- socketio/client/src/App.js
+++ socketio/client/src/App.js
@@ -5,11 +5,17 @@ const socket = io.connect("http://localhost:3001");
 
 
 function App() {
+  const [room, setRoom] = useState("");
   const [message, setMessage] = useState("");
   const [messageReceived, setMessageReceived] = useState("")
 
+  const joinRoom = () => {
+    if (room !== "") {
+      socket.emit("join_room", room)
+    }
+  }
   const sendMessage = () => {
-    socket.emit("send_message", {message: "Hello"})
+    socket.emit("send_message", {message, room})
   };
 
   useEffect(() => {
@@ -20,6 +26,10 @@ function App() {
 
   return (
     <div className="App">
+      <input 
+      placeholder = "Room Number......" 
+      onChange = {(event) => setRoom(event.target.value)} />
+      <button onClick={joinRoom}>Join Room</button>
       <input 
       placeholder = "Message......" 
       onChange = {(event) => setMessage(event.target.value)} />
